8a6c349f731cfeef9a243d452ee7650871d42f27
usr_mzieja <mateusz.zieja@comarch.pl>
2016-04-28 15:51:18 +0200

    setting gulp watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,26 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const sass = require('gulp-sass');
 
-gulp.task('default', () =>
-    gulp.src('src/app.js')
-        .pipe(babel({
-            presets: ['es2015']
-        }))
-        .pipe(gulp.dest('dist'))
+gulp.task('default', ['watch'], () => {
+
+});
+
+gulp.task('es6', () =>
+    gulp.src('src/js/app.js')
+    .pipe(babel({
+        presets: ['es2015']
+    }))
+    .pipe(gulp.dest('dist/js'))
 );
+
+gulp.task('sass', () =>
+    gulp.src('src/sass/style.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist/css'))
+);
+
+gulp.task('watch', () => {
+    gulp.watch('src/sass/*.scss', ['sass']);
+    gulp.watch('src/js/*.js', ['es6']);
+});
